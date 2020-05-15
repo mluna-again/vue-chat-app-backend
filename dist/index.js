320d54eb512943860188c2f7fb4d264073c1e350
Manuel Luna <manuellunada@gmail.com>
2020-05-14 21:04:18 -0500

    Added menu of active users

--- dist/index.js
+++ dist/index.js
@@ -11,11 +11,25 @@ const server = http_1.default.createServer((req, res) => {
 });
 const io = socket_io_1.default(server);
 const port = process.env.PORT || 3000;
+const users = [];
 io.on('connection', (socket) => {
-    console.log('User connected');
+    socket.emit('users-online', users);
     socket.on('new-message', (message) => {
         console.log(message);
         io.emit('message', message);
+    });
+    socket.on('new-user', (user) => {
+        console.log('New user registered: ' + user);
+        users.push({ user: user, userId: socket.id });
+        io.emit('user', user);
+    });
+    socket.on('disconnect', () => {
+        const userLeavingIndex = users.findIndex((u) => u.userId === socket.id);
+        if (userLeavingIndex === -1)
+            return;
+        io.emit('user-disconnect', users[userLeavingIndex].user);
+        console.log('User leave: ' + users[userLeavingIndex].user);
+        users.splice(userLeavingIndex, 1);
     });
 });
 server.listen(port, () => console.log('Listening on port ' + port));
--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,34 @@ const server = http.createServer((req, res) => {
 
 const io = socket(server);
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
+
+const users: any = [];
 
 io.on('connection', (socket) => {
-  console.log('User connected');
+  socket.emit('users-online', users);
+
   socket.on('new-message', (message) => {
     console.log(message);
     io.emit('message', message);
+  });
+
+  socket.on('new-user', (user) => {
+    console.log('New user registered: ' + user);
+    users.push({ user: user, userId: socket.id });
+    io.emit('user', user);
+  });
+
+  socket.on('disconnect', () => {
+    const userLeavingIndex = users.findIndex(
+      (u: any) => u.userId === socket.id
+    );
+
+    if (userLeavingIndex === -1) return;
+
+    io.emit('user-disconnect', users[userLeavingIndex].user);
+    console.log('User leave: ' + users[userLeavingIndex].user);
+    users.splice(userLeavingIndex, 1);
   });
 });
 
